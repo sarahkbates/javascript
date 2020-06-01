ff406bd5035633656f76e32d9595c3375336a0ce
Sarah Bates <skbates00@me.com>
2020-06-01 09:23:59 -0500

    did exercise 3

--- exercise3.js
+++ exercise3.js
@@ -8,20 +8,21 @@
 
 
 //your code...
-
-
-
-
-
+function isEmptyStr(str){
+    return (!str || 0 === str.length);
+}
+console.log(isEmptyStr());
 /************************************************************* */
 
 // Problem 2:
 // Declare a variable that holds a string (e.g., ‘Learning JavaScript is fun!’). Replace a word so that the output uses the new word instead (e.g., ‘Learning JavaScript is cool!). 
-
-
-
 //your code...
 
+
+let myString = "I love all dogs!";
+let res = myString.replace(/all/g, "my");
+console.log(myString);
+console.log(res);
 
 
 
@@ -33,7 +34,9 @@
 
 //your code...
 
-
+let arr = [1,2,3,4,5];
+let squares = arr.map(x => x * x);
+console.log(squares);
 
 
 
@@ -46,7 +49,8 @@
 
 //your code...
 
-
+const myArray = [1,3,5,7,9,1,3,5].filter((x)=> {return x > 3;});
+console.log(myArray);
 
 
 
@@ -59,6 +63,9 @@
 
 //your code...
 
+const arr1 = [1,4,6,8,9];
+const arrSum = arr1.reduce((a,b) => a + b, 0);
+console.log(arrSum);
 
 
 
@@ -72,9 +79,36 @@
 
 
 
+
 //your code...
 
-
+function dnaPair(koalaStr){
+    //return each strand as an array
+const paired = [];
+//fucntion to check with strand to pair
+const search = function(char){
+    switch(char){
+        case "A":
+            paired.push("T");
+            break;
+        case "T":
+            paired.push("A");
+            break;
+        case "C":
+            paired.push("G");
+            break;
+        case "G":
+            paired.push("C");
+            break;
+    }
+};
+//loops through input and pair
+for (let i = 0; i <koalaStr.length; i++){
+    search(koalaStr[i]);
+}
+return paired;
+}
+console.log(dnaPair("GCG CCA GTC CCT CTA GAA GAC TGA"));
 
 
 
@@ -86,14 +120,26 @@
 const numbers = [2,23,1,2,1,1,1,2,2.5,20,200,2000,,{k:1},20000,19999,1878,140,23,4,"sk",true,true,"true-dat","nice","one","two","three","3","tea",[]];
 
 function maxNumber(numbers) {
-    //your code...
-}
+    //your code here
+    //Amir did this one for us
+    const truthys = numbers.filter(el => el);
+    const primatives = truthys.filter(n => typeof n != 'object');
+    const noBools = primatives.filter(p => typeof p != 'boolean');
+    const numStrings = ["zero", "one", "two", "three"];
+    const numWithStrings = noBools.map(nb => numStrings.includes(nb) ? numStrings.indexOf(nb) : nb);
+    const numWithStrings2 = numWithStrings;
+    numWithStrings2[24] = Number (numWithStrings[24]);
+    const nums = numWithStrings2.filter(n => typeof n != 'string');
+    return Math.max(...nums);
+};
+console.log(maxNumber());
 
 // 7.b -Write a function that sorts the given numbers array.  Allow the function to sort the array in descending order
 
 function sortNums(numbers,desc=false) {
-    //your code...
+    points.sort(function(a,b){return b-a});
 };
+console.log(sortNums());
 
 
 
@@ -119,7 +165,7 @@ console.log(mapObj.has({company : "TEKsystems"}));
 
 let ones = [1,11,111,1111,11111,111111,1111111,11111111,111111111,1111111111];
 //reverse the array, without modifying the ones array.
-
+console.log(ones.reverse());
 
 /************************************************************* */
 //Problem 12:
@@ -127,8 +173,16 @@ let ones = [1,11,111,1111,11111,111111,1111111,11111111,111111111,1111111111];
 
 function performer(cb) {
     //code goes here
+    this.cb = cb;
 }
-
+Thing.prototype.doSomething = function (callback){
+    callbacl.call(this);
+}
+function foo(){
+    console.log(this.cb);
+}
+const t = new cb ('Sarah');
+t.doSomething(foo);
 
 /************************************************************* */
 //Bonus assignment:
